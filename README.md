627378c0a4805d2011ca05bd0a033095bbfbe0d1
INF <Yao_icft@outlook.com>
2025-11-28 07:03:37 +0800

    update

--- README.md
+++ README.md
@@ -1,18 +1,17 @@
-### INFL is a Python Lib.
+### INFL
+
+INFL is a Python Lib.
+
+![License](https://img.shields.io/badge/License-MIT-blue.svg)
 
 ---
 
-## How to use it?
+## Use
 
-If you want to use it, please download it and copy to "C:\Users\INFINITIVE\AppData\Local\Programs\Python\Python313\Lib\".
-
-Enter "pip install -r requirements.txt" in the directory where this project is located.
+If you want to use it, please download it and copy to `C:\Users\INFINITIVE\AppData\Local\Programs\Python\Python313\Lib\`.
 
 ---
 
-## 如何使用它？
+## 使用
 
-如果你想使用它，请将其下载并复制到"C:\Users\INFINITIVE\AppData\Loal\Programs\Python\Python313\Lib\"目录下。
-
-在本项目所在目录输入 pip install -r requirements.txt 。
-
+如果你想使用它，请将其下载并复制到 `C:\Users\INFINITIVE\AppData\Loal\Programs\Python\Python313\Lib\`。
--- logger.py
+++ logger.py
@@ -2,21 +2,25 @@
 from typing import Literal
 from time import strftime, localtime
 
-_log_path: str = "./latest.log"
+_log_path: str= "./latest.log"
+with open(_log_path, mode="w+", encoding="utf-8") as log_file:
+    log_file.close()
 
 def init(log_path: str = "./latest.log"):
     global _log_path
-    _log_path: str = log_path
+    _log_path= log_path
     with open(log_path, mode="w+", encoding="utf-8") as log_file:
-        log_file.close()
+        ...
 
 def log(*texts: object,
-        level: Literal["TRACE", "DEBUG", "INFO", "WARN", "ERROR", "FATAL"] = "INFO",
-        thread: str = "Main") -> str:
+        level: Literal["TRACE", "DEBUG", "INFO","WARN", "ERROR", "FATAL"] = "INFO",
+        thread: str = "Main", **kwargs) -> tuple[object]:
     global _log_path
-    text: str = f"[{strftime("%H:%M:%S", localtime())}] [{thread}/{level}]: {" ".join([t.__str__() for t in texts])}"
-    with open(_log_path, mode="a+", encoding="utf-8") as log_file:
+
+    text: str = f"[{strftime("%H:%M:%S", localtime())}] [{thread}/{level\
+        }]: {" ".join([t.__str__() for t in texts])}"
+    with open(_log_path, mode="a", encoding="utf-8") as log_file:
         log_file.write(text + "\n")
-        log_file.close()
-    print(text)
+
+    print(text, **kwargs)
     return texts
